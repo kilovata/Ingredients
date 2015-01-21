a663c67ec8a03b32bc650be238bbed38ef86849c
kilovata <kilovata@gmail.com>
2015-01-21 16:00:01 +0300

    Initial Commit

---
+++ Ingredients/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ingredients
+//
+//  Created by kilovata-iMac on 21/01/15.
+//  Copyright (c) 2015 kilovata-iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
